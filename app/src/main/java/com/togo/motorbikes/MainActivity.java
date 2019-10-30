e58923e247778fd48ca84164d26843e782ede02a
Brian-Orina <brianmogaka.bm@gmail.com>
2019-10-30 14:21:29 +0300

    Initial Commits

---
+++ app/src/main/java/com/togo/motorbikes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.togo.motorbikes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
